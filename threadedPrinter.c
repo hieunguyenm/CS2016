4cd38fa746c080f2a06eb9f15cb8cc7fd6643467
Niall Hunt <nihunt@lg12l8.scss.tcd.ie>
2018-02-15 10:37:27 +0000

    After lab

--- threadedPrinter.c
+++ threadedPrinter.c
@@ -1,38 +1,34 @@
 #include <pthread.h>
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define NUM_CONSUMER_THREADS 3
 #define NUM_TOTAL_THREADS (NUM_CONSUMER_THREADS + 1)
+#define BUFF_SIZE 100;
 
 struct shared_data
 {
   pthread_mutex_t mutex;
-  pthread_cond_t cond1;
-  pthread_cond_t cond2;
-  unsigned int finished:1;
+  pthread_cond_t buf_full;
+  pthread_cond_t buf_empty;
+  int threadUsed;
   unsigned int printed:1;
+  unsigned int toPrint:1;
+  char* input_string;
 };
 
-void* mainThreadFunction(struct shared_data* shared_data);
+void* mainThreadFunction(void* shared_data);
 void* consumerFunction(void* threadId);
 void* printerThreadFunction(void* threadId);
 
 int main (int argc, char** argv)
-{
-  struct shared_data shared_data;
-
-  pthread_mutex_init(&shared_data.mutex, NULL);
-  pthread_cond_init(&shared_data.cond1, NULL);
-  pthread_cond_init(&shared_data.cond2, NULL);
-
-  shared_data.finished = 0;
-  shared_data.printed = 1;
-
+{ 
   // Creating main thread
   pthread_t main_thread;
   printf("Creating main thread\n");
-  int rc = pthread_create(&main_thread, NULL, mainThreadFunction, &shared_data);
+  int rc = pthread_create(&main_thread, NULL, mainThreadFunction, (void*) *argv);
   if(rc)
   {
     printf("ERROR return code from pthread_create(): %d\n", rc);
@@ -45,8 +41,17 @@ int main (int argc, char** argv)
   return 0;
 }
 
-void* mainThreadFunction(struct shared_data* shared_data)
+void* mainThreadFunction(void* argv)
 {
+  struct shared_data shared_data;
+  
+  pthread_mutex_init(&shared_data.mutex, NULL);
+  pthread_cond_init(&shared_data.cond1, NULL);
+  pthread_cond_init(&shared_data.cond2, NULL);
+
+  shared_data.printed = 0;
+  shared_data.toPrint = 0;
+
   printf("Main thread created\n");
 
   pthread_t threads[NUM_TOTAL_THREADS];
@@ -56,7 +61,7 @@ void* mainThreadFunction(struct shared_data* shared_data)
   for(int t = 0;(t < NUM_CONSUMER_THREADS);t++)
   {
     printf("Creating thread\n");
-    rc = pthread_create(&threads[t], NULL, consumerFunction, (void*) t);
+    rc = pthread_create(&threads[t], NULL, consumerFunction, (void*) &shared_data);
     if(rc)
     {
       printf("ERROR return code from pthread_create(): %d\n", rc);
@@ -64,26 +69,54 @@ void* mainThreadFunction(struct shared_data* shared_data)
     }
   }
 
+	
   // Creating printer thread
   printf("Creating printer thread\n");
-  rc = pthread_create(&threads[NUM_TOTAL_THREADS - 1], NULL, printerThreadFunction, (void*) (NUM_TOTAL_THREADS - 1));
+  rc = pthread_create(&threads[NUM_TOTAL_THREADS - 1], NULL, printerThreadFunction, (void*) &shared_data);
   if(rc)
   {
     printf("ERROR return code from pthread_create(): %d\n", rc);
     exit(-1);
   }
-
+  
+	//getLine 
+	char *buffer;
+  size_t bufsize = BUFF_SIZE;
+  
+  int finished = 0;
+  while(!finished)
+  {
+		buffer = (char *) malloc(bufsize * sizeof(char));
+		if(buffer == NULL)
+		{
+			printf("ERROR: U DUM");
+			exit(-1);
+		}
+	
+		getline(&buffer, &bufsize, stdin);
+		
+		if(strcmp("\n", buffer) == 0)
+			finished = 1;
+		
+		pthread_mutex_lock(&shared_data.mutex);
+		strcpy(&shared_data.input_string, &buffer);
+		shared_data.toPrint = 1;
+		pthread_cond_signal (&cond);
+		pthread_mutex_unlock(&shared_data.mutex);
+	}
+	
   pthread_exit(NULL);
 }
 
-void* consumerFunction(void* threadId)
+void* consumerFunction(void* shared_data)
 {
-  printf("Consumer thread id: %d\n", threadId);
+	pthread_mutex_lock(&shared_data->mutex);
+	while(!shared_data->toPrint)
+		pthread_cond_wait(&shared_data->buf_full, &shared_data->mutex);
   pthread_exit(NULL);
 }
 
-void* printerThreadFunction(void* threadId)
+void* printerThreadFunction(void* shared_data)
 {
-  printf("Printer thread id: %d\n", threadId);
   pthread_exit(NULL);
 }
